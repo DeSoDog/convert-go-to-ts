d6e8c484c1ce14109477dd00baac73d674faa6e4
DeSoDog <ty@deso.org>
2022-02-28 17:21:11 -0600

    Cleanup

--- src/convert-to-ts.ts
+++ src/convert-to-ts.ts
@@ -3,7 +3,6 @@ import { exec } from "child_process";
 import fs from "fs";
 const main = () => {
   exec(`go run ..\\..\\to-ts.go`, (err, stdout, stderr) => {
-    // console.log("oy", stdout);
     fs.writeFile("blob.ts", stdout, (err) => {
       console.log(err);
     });
